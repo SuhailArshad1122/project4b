b421b66417f4ddab0bac98a6f087c402829bbe24
Suhail <SuhailArshad1122@gmail.com>
2020-07-21 23:32:20 +0500

    Project4b

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import useWebAnimations from "@wellyshen/use-web-animations";
+
 
 function App() {
+
+  const { ref, playState } = useWebAnimations({
+    keyframes: [{transform: "translate(0,0)"}, {transform: "translate(750px,0)"}],
+    timing: {
+      delay: 500, // Start with a 500ms delay
+      duration: 3000, // Run for 1000ms
+      iterations: Infinity, // Repeat once
+      direction: "alternate", // Run the animation forwards and then backwards
+      easing: "ease-in-out", // Use a fancy timing function
+      
+    },
+    onReady: ({ playState, animate, animation }) => {
+      // Triggered when the animation is ready to play (Google Chrome: available in v84+)
+    },
+    onUpdate: ({ playState, animate, animation }) => {
+      // Triggered when the animation enters the running state or changes state
+      
+    },
+    onFinish: ({ playState, animate, animation }) => {
+      // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='target' ref={ref}>
+        <img src='./PakFlag.png'/>
+      </div>
     </div>
   );
 }
